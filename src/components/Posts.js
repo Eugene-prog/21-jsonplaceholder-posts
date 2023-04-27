4164cc2c6a6e484ec3203e57c650ac53a3e85e95
Eugene-prog <ieugene.prog@gmail.com>
2023-04-27 21:42:06 +0700

    feat: getting a list of posts

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,6 +1,18 @@
 import Post from './Post';
+import { useState, useEffect } from 'react';
 
 function Posts() {
+  const[posts, setPosts] = useState([]);
+
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/posts/')
+      .then(response => response.json())
+      .then(posts => {
+        console.log(posts);
+        setPosts(posts)
+      })
+  }, [])
+
   return(
     <div>
       <Post/>
