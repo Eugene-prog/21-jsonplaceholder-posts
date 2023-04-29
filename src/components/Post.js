97f2e51f919d7f20ce06c73c73b5430a193eaa5e
Eugene-prog <ieugene.prog@gmail.com>
2023-04-29 16:55:38 +0700

    feat: add display each post from the posts array on the screen with a unique key and pass props

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,9 +1,13 @@
-function Post() {
-
-  return (
-    <h1>Post</h1>
-  )
+function Post(props) {
+  const {id, title, userId, body} = props;
   
+  return (
+    <div key={id} className='post'>
+      <p>user: {userId}</p>
+      <h2>{title}</h2>
+      <p>{body}</p>
+    </div>
+  )
 }
 
 export default Post;
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -2,24 +2,19 @@ import Post from './Post';
 import { useState, useEffect } from 'react';
 
 function Posts() {
-  const[posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/posts/')
-      .then(response => response.json())
-      .then(posts => {
+      .then((response) => response.json())
+      .then((posts) => {
         console.log(posts);
-        setPosts(posts)
+        setPosts(posts);
       })
-  }, [])
+      .catch((error) => console.log(error.message));
+  }, []);
 
-  return(
-    <div>
-      <Post/>
-      <Post/>
-      <Post/>
-    </div>
-  )
+  return <div>{posts.length > 0 && posts.map((post) => <Post key={post.id} {...post} />)}</div>;
 }
 
 export default Posts;
